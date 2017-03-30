436ea22e175b29f39ca7cff75f0b4ebb8dd9437f
HDYA <hdya_backfire@sina.cn>
2017-03-30 17:41:57 +0800

    Add layer and buttons for game and demo mode

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,6 +1,8 @@
 /**
  * Created by HDYA-Backfire on 2017-03-23.
  */
+var $layer;
+
 $(function () {
     var map1 = new Map($('.map'), 50, 70);
     window.onresize = function() {
@@ -8,13 +10,34 @@ $(function () {
     };
     map1.initiate();
 
-    startLocalAnimation(map1, 10);
+    $layer = $('.layer');
+
+    $('#start').click(function() {
+        startGame();
+        hideLayer();
+    });
+    $('#local').click(function() {
+        startLocalAnimation(map1, 10);
+        hideLayer();
+    });
+    $('#remote').click(function() {
+        startLocalAnimation(map1, 10);
+        hideLayer();
+    });
 });
+
+function hideLayer() {
+    $layer.hide();
+}
 
 function startClock(map) {
     window.setInterval(function () {
         map.moveAllSnake();
     }, 100);
+}
+
+function startGame() {
+    alert('Sorry, Not Implemented Yet!');
 }
 
 function startLocalAnimation(map, number) {
